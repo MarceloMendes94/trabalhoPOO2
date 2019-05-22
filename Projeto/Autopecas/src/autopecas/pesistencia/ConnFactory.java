ee7dd098225344c5fc46ef6b2000fe158f95f71b
marcelo <marceloestudoifes@gmail.com>
2019-05-22 14:42:52 -0300

    inserção de porta padrão ao projeto

--- Projeto/Autopecas/src/autopecas/pesistencia/ConnFactory.java
+++ Projeto/Autopecas/src/autopecas/pesistencia/ConnFactory.java
@@ -3,11 +3,8 @@ package autopecas.pesistencia;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
-
-
 public class ConnFactory {
-       private final String url = "jdbc:postgresql://localhost:45453/banana";//"jdbc:postgresql://stampy.db.elephantsql.com:5432/pvunmqpv"
+       private final String url = "jdbc:postgresql://localhost:5432/banana";//"jdbc:postgresql://stampy.db.elephantsql.com:5432/pvunmqpv"
        private final String user = "postgres";//"pvunmqpv"
        private final String password = "aluno";//"rX3CKCsGeqAIlEut5W2HeMxF8f-uyYNA
 
@@ -15,7 +12,7 @@ public class ConnFactory {
        public synchronized void teste(){
             Connection c = null;   
             try{
-                c = DriverManager.getConnection("jdbc:postgresql://localhost:5453/banana","postgres", "aluno");   
+                c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "aluno");   
                 System.out.println("Opened database successfully"); 
             }catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());            
